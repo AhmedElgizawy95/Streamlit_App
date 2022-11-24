833436da305343b6621801170fb12e1375e9a661
Ahmed Alaa <85286691+AhmedElgizawy95@users.noreply.github.com>
2022-11-24 11:51:24 +0200

    Update streamlit_cctv.py

--- streamlit_cctv.py
+++ streamlit_cctv.py
@@ -13,24 +13,24 @@ from PIL import Image
 
 
 # In[2]:
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
+# def get_base64(bin_file):
+#     with open(bin_file, 'rb') as f:
+#         data = f.read()
+#     return base64.b64encode(data).decode()
 
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    .stApp {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
+# def set_background(png_file):
+#     bin_str = get_base64(png_file)
+#     page_bg_img = '''
+#     <style>
+#     .stApp {
+#     background-image: url("data:image/png;base64,%s");
+#     background-size: cover;
+#     }
+#     </style>
+#     ''' % bin_str
+#     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-set_background('download2.png')
+# set_background('download2.png')
 #page_bg_img = '''
 #<style>
 #.stApp{
@@ -41,8 +41,8 @@ set_background('download2.png')
 #'''
 
 #st.markdown(page_bg_img, unsafe_allow_html=True)
-#image = Image.open('download.jpg')
-#st.image(image, use_column_width=200)
+image = Image.open('download.jpg')
+st.image(image, use_column_width=200)
 
 
 
